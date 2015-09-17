fdc6a800da29c7f44d7cfeb45315ce234a06e31e
willianrlm2 <willianw94@hotmail.com>
2015-09-17 20:48:19 -0300

    Muitas coisas cara v5

--- cabecalho.php
+++ cabecalho.php
@@ -1,21 +1,27 @@
-<!DOCTYPE html>
-<html>
 <head>
     <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
-    <meta charset="utf-8">
-    <title>SM Team Fight</title>
+    <meta charset="utf-8"/>
+
+    <!--Bootstrap 4 alpha-->
+    <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+    
     <!-- Css SM TEAN FIGHT -->
     <link href="css/style.css" rel="stylesheet">
+
+    <title>SM Team Fight</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Academia de muay thay e taekwondo">
+    <meta name="keywords" content="sm team fight, muay thai, muay, muay boran, taekwondo">
+    <meta name="author" content="SM Team Fight">
     <!-- Bootstrap --><!--
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/min.js" type="text/javascript"></script>
     <script src="js/script.js"></script>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>-->
-    <!--Bootstrap 4 alpha-->
-    <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
 </head>
 <body style="background-color: #262626; color: white">
 <nav class="navbar navbar-dark bg-inverse" style="border-radius: 0%; background-color: #000000;">
@@ -24,7 +30,7 @@
     </button>
     <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
         <a class="navbar-brand" href="#">
-            <img src="img/logo.jpg" width="50px">
+            <img src="img/logo.png" width="50px">
         </a>
         <? if (!isset($_SESSION['chave_sm_team'])) { ?>
             <ul class="nav navbar-nav">
--- index.php
+++ index.php
@@ -1,48 +1,46 @@
-<?php
+<?
+include 'service/Conexao.php';
 include 'cabecalho.php';
 ?>
+<style>
+    #menu_principal {
+        position: fixed;
+        width: 100%;
+        z-index: 999
+    }
+</style>
 <main>
     <section class="module parallax parallax-1">
-        <div class="container">
-            <h1>SM Team Fight</h1>
+        <div class="container" id="logo_inicial">
+            <h1></h1>
         </div>
     </section>
     <section class="module content" id="Apresentação">
         <div class="container">
             <h2>Notícias / Informações</h2>
 
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"">
                 <ol class="carousel-indicators">
                     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
                     <li data-target="#carousel-example-generic" data-slide-to="2"></li>
                 </ol>
-                <div class="carousel-inner" role="listbox" style="height: 400px">
+                <div class="carousel-inner" role="listbox" style="max-height: 500px;">
                     <div class="carousel-item active">
-                        <img src="img/demo/_small/1.jpg" alt="..." height="300px">
+                        <img src="img/slides/prop1.jpg" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/slides/taekwondo.jpg" alt="...">
 
                         <div class="carousel-caption">
-                            <h3>SM Team Fight</h3>
-
-                            <p>...</p>
+                            <h2>Taekwondo WTF</h2>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="img/demo/_small/2.jpg" alt="..." height="300px">
+                        <img src="img/slides/muay5.jpg" alt="...">
 
                         <div class="carousel-caption">
-                            <h3>Logomarca</h3>
-
-                            <p>...</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/prop1.jpg" alt="..." height="300px">
-
-                        <div class="carousel-caption">
-                            <h3>Outra foto</h3>
-
-                            <p>...</p>
+                            <h2>Muay Thai / Muay Boran</h2>
                         </div>
                     </div>
                 </div>
@@ -58,39 +56,19 @@ include 'cabecalho.php';
         </div>
     </section>
 
-    <section class="module parallax parallax-2" id="Sobre">
+    <section class="module parallax parallax-2" id="Razao">
         <div class="container">
-            <h1>Sobre</h1>
+            <h1>Razão</h1>
         </div>
     </section>
     <section class="module content">
         <div class="container">
-            Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser
-            entendido como manifestação linguística das ideias de um autor, que serão interpretadas pelo leitor de
-            acordo com
-            seus conhecimentos linguísticos e culturais. Seu tamanho é variável.
-
-            “Conjunto de palavras e frases articuladas, escritas sobre qualquer suporte”[1] .
-
-            “Obra escrita considerada na sua redação original e autêntica (por oposição a sumário, tradução, notas,
-            comentários,
-            etc.)”[2] .
-
-            "Um texto é uma ocorrência linguística, escrita ou falada de qualquer extensão, dotada de unidade
-            sociocomunicativa,
-            semântica e formal. É uma unidade de linguagem em uso."[3]
-
-            O interesse pelo texto como objeto de estudo gerou vários trabalhos importantes de teóricos da Linguística
-            Textual,
-            que percorreram fases diversas cujas características principais eram transpor os limites da frase
-            descontextualizada
-            da gramática tradicional e ainda incluir os relevantes papéis do autor e do leitor na construção de textos.
-
-            Um texto pode ser escrito ou oral e, em sentido lato, pode ser também não verbal.
-
-            Texto crítico é uma produção textual que parte de um processo reflexivo e analítico gerando um conteúdo com
-            crítica
-            construtiva e bem fundamentada.
+            Aulas de Muaythai e Taekwondo WTF, venda de materiais a Pronta Entrega de Muaythai e Taekwondo, produtos
+            excelentes e preços acessíveis é no SM Team Fight/KeepFit, ambiente climatizado e sala específica para Artes
+            Marciais,
+            com profissionais especializados e todos os materiais necessários para um treino com a máxima segurança,
+            nosso endereço é no 4º Andar do Shopping Santa Maria Calçadão no centrão de Santa Maria,
+            venha fazer uma aula experimental gratuita!!!<br>
         </div>
     </section>
     <section class="module parallax parallax-3" id="Cadastro">
@@ -101,57 +79,95 @@ include 'cabecalho.php';
     <section class="module content">
         <div class="container">
             <form action="controller/Controller.php" method="post">
-                <fieldset class="form-group">
-                    <label for="formGroupExampleInput">Nome</label>
+                <div class="col-md-12">
+                    <div style="color: red;">
+                        <strong>*</strong> Campos marcados são obrigatórios!<hr>
+                    </div>
+                </div>
+                <fieldset class="form-group col-md-12">
+                    <label for="formGroupExampleInput">Nome *</label>
                     <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome completo">
                 </fieldset>
-                <fieldset class="form-group">
-                    <label for="formGroupExampleInput2">E-mail</label>
+                <fieldset class="form-group col-md-12">
+                    <label for="formGroupExampleInput2">E-mail *</label>
                     <input type="email" class="form-control" id="email" name="email" placeholder="E-mail para contato">
                 </fieldset>
-                <fieldset class="form-group">
-                    <label for="formGroupExampleInput">Senha</label>
+                <fieldset class="form-group col-md-6">
+                    <label for="formGroupExampleInput">Senha *</label>
                     <input type="password" class="form-control" id="senha" name="senha" placeholder="Senha para acesso">
                 </fieldset>
-                <fieldset class="form-group">
-                    <label for="formGroupExampleInput2">Endereço</label>
+                <fieldset class="form-group col-md-6">
+                    <label for="formGroupExampleInput">Confirme a senha *</label>
+                    <input type="password" class="form-control" id="senha2" name="senha2"
+                           placeholder="Confirmação de senha">
+                </fieldset>
+                <fieldset class="form-group col-md-12">
+                    <label for="formGroupExampleInput2">Endereço *</label>
                     <input type="text" class="form-control" id="endereco" name="endereco"
                            placeholder="Endereço para contato">
                 </fieldset>
-                <fieldset class="form-group">
+                <fieldset class="form-group col-md-4">
                     <label for="formGroupExampleInput">Telefone</label>
                     <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Telefone fixo">
                 </fieldset>
-                <fieldset class="form-group">
+                <fieldset class="form-group col-md-4">
                     <label for="formGroupExampleInput2">Celular</label>
                     <input type="tel" class="form-control" id="celular" name="celular" placeholder="Celular">
                 </fieldset>
-                <fieldset class="form-group">
+                <fieldset class="form-group col-md-4">
+                    <label for="formGroupExampleInput2">Data de nascimento *</label>
+                    <input type="date" class="form-control" id="data" name="data" placeholder="Data de nascimento">
+                </fieldset>
+                <fieldset class="form-group col-md-12">
                     <label for="formGroupExampleInput2">Profissão</label>
                     <input type="text" class="form-control" id="profissao" name="profissao" placeholder="Profissão">
                 </fieldset>
-                <fieldset class="form-group">
+                <fieldset class="form-group col-md-12">
                     <label for="formGroupExampleInput2">Horários de trabalho</label>
                     <input type="text" class="form-control" id="horarios" name="horarios"
                            placeholder="Horários de trabalho">
+                    <hr>
+
+                    <button class="btn btn-success-outline" name="operacao" value="cadastrar_usuario">
+                        Cadastrar
+                    </button>
                 </fieldset>
-                <fieldset class="form-group">
-                    <label for="formGroupExampleInput2">Data de nascimento</label>
-                    <input type="date" class="form-control" id="data" name="data" placeholder="Data de nascimento">
-                </fieldset>
-                <button class="btn btn-success-outline" name="operacao" value="cadastrar_usuario">
-                    Cadastrar
-                </button>
             </form>
         </div>
+        <hr>
     </section>
 
-    <div class="col-md-12">
+    <div class="col-md-12" id="Informacoes" style="border: 1px solid green; border-radius: 5px; margin-bottom: 50px">
         <div class="container">
-            <h1>Contato</h1>
+            <br><h1>Informações</h1>
         </div>
-        <div class="container col-md-offset-3" style="margin-bottom: 50px">
-            (xx) xxxx-xxxx / (xx) xxxx-xxxx / xxx@mail.com.br
+        <div class="container col-md-offset-1" style="margin-bottom: 50px">
+            <h4>Funcionamento</h4>
+            De segunda a sexta das 14h ás 22 h.
+            Sábados das 15 h as 20 h.
+            <hr>
+            <h4>Endereço</h4>
+            Dr Bozzano, 1263 - 4° Andar Calçadão Shopping Santa Maria - Centro, 97015-004 Santa Maria.
+            <hr>
+            <h4>Contato</h4>
+            55 3347-3387 / 55 9906-0410 <img src="img/logos/whatsapp.png" width="25px" style="margin-bottom: 5px"
+                                             title="Whatsapp" alt="Whatsapp"><br>
+            E-mail: crismoraes_boton@hotmail.com
+            <hr>
+        </div>
+        <div class="container col-md-offset-1" style="margin-bottom: 50px">
+            <div class="fb-like"></div>
+            <div id="fb-root"></div>
+            <script>
+                (function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.4";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            </script>
         </div>
     </div>
 </main><!-- /main -->
